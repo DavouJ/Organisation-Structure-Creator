06562eaced913e02f68d713495c93f8e4569b6f9
DavouJ <djjobbi@hotmailcom>
2024-02-13 20:02:36 +0000

    added inquirer options for user and write file functionality

--- starter/index.js
+++ starter/index.js
@@ -38,8 +38,8 @@ function createManager() {
         },
         {
             type: 'number',
-            name: 'id',  
-            message: 'Enter the team manager\'s ID.' ,
+            name: 'id',
+            message: 'Enter the team manager\'s ID.',
             validate: (answer) => {
                 if (answer !== '') {
                     return true;
@@ -76,17 +76,19 @@ function createManager() {
             }
         }
     ])
-    .then((managerAnswers) => {
-        // Then store the answers into a new Manager object
-        const manager = new Manager(managerAnswers.name, managerAnswers.id, managerAnswers.email, managerAnswers.officeNumber);
-        //push the manager into the team members array
-        teamMembers.push(manager)
-    })
-}    
+        .then((managerAnswers) => {
+            // Then store the answers into a new Manager object
+            const manager = new Manager(managerAnswers.name, managerAnswers.id, managerAnswers.email, managerAnswers.officeNumber);
+            //push the manager into the team members array
+            teamMembers.push(manager)
+            next()
+        })
+    
+}
 
 // Function to create an engineer.
-const createEngineer = () => { 
-    
+const createEngineer = () => {
+
     // Asks for a name, id, email, and github
     return inquirer.prompt([
         {
@@ -104,7 +106,7 @@ const createEngineer = () => {
         },
         {
             type: 'number',
-            name: 'id',  
+            name: 'id',
             message: 'Enter the team engineers\'s ID.',
             validate: (answer) => {
                 if (answer !== '') {
@@ -142,92 +144,117 @@ const createEngineer = () => {
             }
         }
     ])
-    .then((engineerAnswers) => {
-        // Then store the answers into a new engineer object
-        const engineer = new Engineer(engineerAnswers.name, engineerAnswers.id, engineerAnswers.email, engineerAnswers.gitHub);
-        //push the manager into the team members array
-        teamMembers.push(engineer)
-    })
-}    
+        .then((engineerAnswers) => {
+            // Then store the answers into a new engineer object
+            const engineer = new Engineer(engineerAnswers.name, engineerAnswers.id, engineerAnswers.email, engineerAnswers.gitHub);
+            //push the manager into the team members array
+            teamMembers.push(engineer)
+            next()
+        })
+    
+}
 
 // Function to create an intern
-    // Asks for a name, id, email, and school
-    const createInter = () => { 
-    
-        // Asks for a name, id, email, and github
-        return inquirer.prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: 'What is the team interns\'s name?',
-                validate: (answer) => {
-                    if (answer !== '') {
-                        return true;
-                    } else {
-                        console.log('Please enter the team interns\'s name');
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'number',
-                name: 'id',  
-                message: 'Enter the team interns\'s ID.',
-                validate: (answer) => {
-                    if (answer !== '') {
-                        return true;
-                    } else {
-                        console.log('Please enter the team interns\'s name');
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: 'What is the team interns\'s email address?',
-                validate: (answer) => {
-                    if (answer !== '') {
-                        return true;
-                    } else {
-                        console.log('Please enter the team interns\'s name');
-                        return false;
-                    }
-                }
-            },
-            {
-                type: 'input',
-                name: 'gitHub',
-                message: 'What is the team interns\'s GitHub username?',
-                validate: (answer) => {
-                    if (answer !== '') {
-                        return true;
-                    } else {
-                        console.log('Please enter the team interns\'s name');
-                        return false;
-                    }
+// Asks for a name, id, email, and school
+const createInter = () => {
+
+    // Asks for a name, id, email, and github
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the team interns\'s name?',
+            validate: (answer) => {
+                if (answer !== '') {
+                    return true;
+                } else {
+                    console.log('Please enter the team interns\'s name');
+                    return false;
                 }
             }
-        ])
+        },
+        {
+            type: 'number',
+            name: 'id',
+            message: 'Enter the team interns\'s ID.',
+            validate: (answer) => {
+                if (answer !== '') {
+                    return true;
+                } else {
+                    console.log('Please enter the team interns\'s name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the team interns\'s email address?',
+            validate: (answer) => {
+                if (answer !== '') {
+                    return true;
+                } else {
+                    console.log('Please enter the team interns\'s name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'gitHub',
+            message: 'What is the team interns\'s GitHub username?',
+            validate: (answer) => {
+                if (answer !== '') {
+                    return true;
+                } else {
+                    console.log('Please enter the team interns\'s name');
+                    return false;
+                }
+            }
+        }
+    ])
         .then((internAnswers) => {
             // Then store the answers into a new engineer object
             const intern = new Engineer(internAnswers.name, internAnswers.id, internAnswers.email, internAnswers.gitHub);
             //push the manager into the team members array
             teamMembers.push(intern)
+            next()
         })
-    }    
     
+}
 
+function next() {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do next?',
+            name: 'next',
+            choices: ['Add an engineer', 'Add an intern', 'Finish building the team'],
+        }
+    ]).then((nextAnswer) => {
+        switch(nextAnswer.next) {
+            case "Add an engineer":
+                addEngineer();
+                break;
+            case "Add an intern":
+                addIntern();
+                break;
+            case "Finish building the team":
+                createTeam();
+                fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+        }
+    })
+}
 
 // Function to ask the user what team member they would like to create next, either a Engineer or Intern. Also ask if they don't want to add any more team members. SHOULD BE CALLED AT THE END OF EACH MEMBER FUNCTION SO THEY CAN CREATE ANOTHER MEMBER AFTER ALREADY CREATING ONE.
 
-    // Then create a conditional to check which member the user picked and run the appropriate function based off of that input ex:`if(userChoice === 'Engineer'){createEngineer()}`
-    // Have a else condition so that if they choose to not make any more members, the file gets written.(Can create a function for this and then call the function)
+// Then create a conditional to check which member the user picked and run the appropriate function based off of that input ex:`if(userChoice === 'Engineer'){createEngineer()}`
+// Have a else condition so that if they choose to not make any more members, the file gets written.(Can create a function for this and then call the function)
 
 // Function to hold all of the other functions.
-    // Call the function to initialize the app.
-    function driver () {
-        createManager()
-    }
-    
-    driver()
+// Call the function to initialize the app.
+function driver() {
+    createManager()
+}
+
+driver()
